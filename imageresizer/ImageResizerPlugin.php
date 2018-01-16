99799f7e70bebb2ced99cf837de6f2cc58748112
Sven Jungnickel <sven@verbb.io>
2018-01-16 16:06:16 +1100

    version number

--- imageresizer/ImageResizerPlugin.php
+++ imageresizer/ImageResizerPlugin.php
@@ -14,7 +14,7 @@ class ImageResizerPlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '1.0.2';
+        return '1.1.0';
     }
 
     public function getSchemaVersion()
